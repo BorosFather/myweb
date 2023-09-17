d6e785bfbb25467abb0f77130ca104bcd24b323f
BorosFather <boros.zoli024@gmail.com>
2023-09-17 20:01:15 +0200

    navbar hozzaadva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ModellComponent } from './modell/modell.component';
+import { ProjectComponent } from './project/project.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "modell", component: ModellComponent},
+  {path: "project", component: ProjectComponent},
+  {path: "about", component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
